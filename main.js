eb7b0407d7d1589a0635f12fac1689cbd1ac7628
Petter Amland <pokepetter@gmail.com>
2021-05-21 02:15:56 +0200

    turned Entity into a class.

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 scene_1 = Scene('scene_1', {color:'red', children:[
+    health_bar = new HealthBar({max:50, default:25}),
     Button({color:'black'}),
-    Button({color:'yellow'}),
+    b = Button({color:'yellow', scale:.75, on_click:function(){print('v', health_bar); health_bar.value += 5}}),
     ]})
-scene_2 = Scene('scene_2', {color:'blue', x:.25, text:'door'})
+scene_2 = Scene('blue_valley', {color:'blue', text:'door'})
 // state_handler.states['scene_1'] = Entity({color:'red'})
-goto_scene(scene_2)
+goto_scene(scene_1)
 
 Button({text:'1', x:.0, y:-.8, on_click:function(){goto_scene(scene_1)}})
 Button({text:'2', x:.2, y:-.8, on_click:function(){goto_scene(scene_2)}})
