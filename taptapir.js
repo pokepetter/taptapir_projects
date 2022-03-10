add7161e99a2587a8965ab996bb0d408f52f3d85
Petter Amland <petter@rain-games.com>
2022-03-10 12:36:29 +0100

    ...

--- taptapir.js
+++ taptapir.js
@@ -1034,11 +1034,11 @@ function drawScene(gl, programInfo, buffers) {
   const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
   const zNear = 0.1;
   const zFar = 100.0;
-  const projectionMatrix = mat4.create();
+  const projectionMatrix = glMatrix.mat4.create();
 
   // note: glmatrix.js always has the first argument
   // as the destination to receive the result.
-  mat4.perspective(projectionMatrix,
+  glMatrix.mat4.perspective(projectionMatrix,
                    fieldOfView,
                    aspect,
                    zNear,
@@ -1046,11 +1046,11 @@ function drawScene(gl, programInfo, buffers) {
 
   // Set the drawing position to the "identity" point, which is
   // the center of the scene.
-  const modelViewMatrix = mat4.create();
+  const modelViewMatrix = glMatrix.mat4.create();
 
   // Now move the drawing position a bit to where we want to
   // start drawing the square.
-  mat4.translate(modelViewMatrix,     // destination matrix
+  glMatrix.mat4.translate(modelViewMatrix,     // destination matrix
                  modelViewMatrix,     // matrix to translate
                  [-0.0, 0.0, -6.0]);  // amount to translate
   // Tell WebGL how to pull out the positions from the position
