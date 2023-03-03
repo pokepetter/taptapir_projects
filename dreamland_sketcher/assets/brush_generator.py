755fc0b2c69ae464b1d68cf4c248ab3cd248fb45
pokepetter <pokepetter@gmail.com>
2023-03-03 04:08:34 +0100

    ...

---
+++ dreamland_sketcher/assets/brush_generator.py
@@ -0,0 +1,23 @@
+import base64
+import json
+
+
+brushes = dict(
+    round_bitmap_brush = ["round_brush_16.png", ],
+    poke_brush = ["pokebrush.png", ],
+    painterly_brush = ["RGBA anim 01.png", "RGBA anim 02.png", "RGBA anim 03.png", "RGBA anim 04.png"],
+)
+
+brushes_as_strings = {}
+for key, value in brushes.items():
+    images = []
+    for image_name in value:
+        with open(image_name, "rb") as image_file:
+            prefix = f'data:image/png;base64,'
+            data = base64.b64encode(image_file.read()).decode('utf-8')
+            brushes_as_strings[image_name] = prefix + data
+
+json_object = json.dumps(brushes_as_strings, indent=4)
+
+with open('base_64_brushes', 'w', encoding='utf-8') as file:
+    file.write('brush_textures = ' + json_object)
