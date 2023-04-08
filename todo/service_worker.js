f824c5b2013e03afb4c6046e0aa98fd6c89c87f8
pokepetter <pokepetter@gmail.com>
2023-04-08 22:54:00 +0200

    ...

---
+++ todo/service_worker.js
@@ -0,0 +1,16 @@
+const CACHE_NAME  = 'todo_list';
+const CONTENT_TO_CACHE = [
+  '/',
+  '/todo_list.html',
+  '../taptapir/taptapir.js',
+  '../taptapir/sunsnake_compiler.js',
+]
+
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
+  e.waitUntil((async () => {
+    const cache = await caches.open(CACHE_NAME );
+    console.log('[Service Worker] Caching all: app shell and content');
+    await cache.addAll(CONTENT_TO_CACHE);
+  })());
+});
