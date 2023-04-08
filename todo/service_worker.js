d1251a13dbe67e24d667ff56012b5448f7a058b5
pokepetter <pokepetter@gmail.com>
2023-04-08 23:36:39 +0200

    ...

--- todo/service_worker.js
+++ todo/service_worker.js
@@ -1,47 +1,16 @@
-// This is the "Offline page" service worker
+const CACHE_NAME  = 'todo_list-v1';
+const CONTENT_TO_CACHE = [
+  '/',
+  '/todo_list.html',
+  '../taptapir/taptapir.js',
+  '../taptapir/sunsnake_compiler.js',
+]
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-
-const CACHE = "todo_list";
-
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "todo_list.html";
-
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
-});
-
-self.addEventListener('install', async (event) => {
-  event.waitUntil(
-    caches.open(CACHE)
-      .then((cache) => cache.add(offlineFallbackPage))
-  );
-});
-
-if (workbox.navigationPreload.isSupported()) {
-  workbox.navigationPreload.enable();
-}
-
-self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith((async () => {
-      try {
-        const preloadResp = await event.preloadResponse;
-
-        if (preloadResp) {
-          return preloadResp;
-        }
-
-        const networkResp = await fetch(event.request);
-        return networkResp;
-      } catch (error) {
-
-        const cache = await caches.open(CACHE);
-        const cachedResp = await cache.match(offlineFallbackPage);
-        return cachedResp;
-      }
-    })());
-  }
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
+  e.waitUntil((async () => {
+    const cache = await caches.open(CACHE_NAME );
+    console.log('[Service Worker] Caching all: app shell and content');
+    await cache.addAll(CONTENT_TO_CACHE);
+  })());
 });
