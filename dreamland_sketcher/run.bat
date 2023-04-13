20d6ff6c894b01f78c5d34abcfd557da315e9b8c
Petter Amland <petter@rain-games.com>
2023-04-13 14:20:45 +0200

    renamed dreamland sketcher -> otosopp.

--- dreamland_sketcher/run.bat
+++
@@ -1,1 +0,0 @@
-"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" %cd%\dreamland_sketcher.html --disable-web-security --user-data-dir="%cd%\Temp"
---
+++ otosopp/run.bat
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" %cd%\otosopp.html --disable-web-security --user-data-dir="%cd%\Temp"
